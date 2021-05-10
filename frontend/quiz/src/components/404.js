0307cd79a416f78dd41a355e92cf8474b99a3b5e
manralparas <manralparas@gmail.com>
2021-05-10 14:49:43 +0530

    create not found page

---
+++ frontend/quiz/src/components/404.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+            <div style={{margin:"300px"}}>
+
+            </div>
+            <h3 style={{textAlign:"center"}}>Page not found Or you are not authorized</h3>
+        </div>
+    )
+}
+
+export default NotFound;
